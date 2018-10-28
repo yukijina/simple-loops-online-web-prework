651220e7bda0b0e0ee9478a33c2ab65e267ab4c3
Yuki N <lunarbase@outlook.com>
2018-10-28 18:27:15 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,6 +3,7 @@
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   loop do 
+    puts "Welcome to "
     
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
