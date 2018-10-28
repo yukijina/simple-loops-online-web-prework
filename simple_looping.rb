fd5270105433f43839c8991528615d3e909929b2
Yuki N <lunarbase@outlook.com>
2018-10-28 18:28:56 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   loop do 
     puts = "Welcome to Flatiron School's Web Development Course!"
     
-    if counter < number_of_times
+    if counter > number_of_times
       break
     end
 end
